a22aa02a47d04885d3f88a9afbf6d2844aa7a819
Teeranatt <teeranat.sr.61@ubu.ac.th>
2020-10-07 10:39:38 +0700

    Updates

---
+++ precache-manifest.bc9306ea61d5da110392a7445415fcdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1fd11b43ee3e24698f2ee0764238f4",
+    "url": "/com-math-pj-/index.html"
+  },
+  {
+    "revision": "b5b00167409430abb46d",
+    "url": "/com-math-pj-/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93d47b9ae5312a27d880",
+    "url": "/com-math-pj-/static/js/2.febcc973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/com-math-pj-/static/js/2.febcc973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b00167409430abb46d",
+    "url": "/com-math-pj-/static/js/main.6a434411.chunk.js"
+  },
+  {
+    "revision": "83fb70f3499f5d8ed36f",
+    "url": "/com-math-pj-/static/js/runtime-main.6114212d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/com-math-pj-/static/media/logo.5d5d9eef.svg"
+  }
+]);
